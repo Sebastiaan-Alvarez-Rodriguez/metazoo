ad2fd09994e7943c3555440bada700dcfc20da5d
Mariska <mariska-ijpelaar@hotmail.com>
2020-10-24 17:15:04 +0200

    created throughput client

--- metazoo/experiments/throughput.py
+++ metazoo/experiments/throughput.py
@@ -1,18 +1,6 @@
-import os
-import random
-import threading
-import time
-
-from experiments.interface import ExperimentInterface
-import remote.util.balancer as balancer
-import util.fs as fs
-import util.location as loc
-from util.printer import *
-import util.time as tm
-
-class FaultToleranceExperiment(ExperimentInterface):
+class ThroughputExperiment(ExperimentInterface):
     '''
-    Experiment measuring operations per 0.3 second of saturated servers
+    Experiment measuring throughput performance of saturated servers
     as server nodes get killed
     '''
     def num_servers(self):
@@ -38,13 +26,6 @@ class FaultToleranceExperiment(ExperimentInterface):
     def clients_core_affinity(self):
         '''Amount of client processes which may be mapped on the same physical node'''
         return 8
-# prc / prcs per core = allocation
-# 256 /             8 = 32
-# 250 /            10 = 25
-# 260 /            13 = 20 
-# 252 /            14 = 18
-# 256 /            16 = 16
-# 250 /            25 = 10
 
     def server_periodic_clean(self):
         '''Period in seconds for servers to clean their crawlspaces. 0 means no cleaning'''
@@ -58,9 +39,9 @@ class FaultToleranceExperiment(ExperimentInterface):
             fs.join(loc.get_build_dir(), 'zookeeper-3.3.0.jar'),
         ]
 
-        fs.mkdir(fs.join(loc.get_metazoo_dep_dir(), 'faulttolerance_client'), exist_ok=True)
+        fs.mkdir(fs.join(loc.get_metazoo_dep_dir(), 'throughput_client'), exist_ok=True)
         for path in locations:
-            dst = fs.join(loc.get_metazoo_dep_dir(), 'faulttolerance_client', fs.basename(path))
+            dst = fs.join(loc.get_metazoo_dep_dir(), 'throughput_client', fs.basename(path))
             if not fs.exists(dst):
                 fs.ln(path, dst)
 
@@ -68,21 +49,8 @@ class FaultToleranceExperiment(ExperimentInterface):
     def pre_experiment(self, metazoo):
         self._prepare_classpath_symlinks()
         '''Execution before experiment starts. Executed on the remote once.'''
-        metazoo.register['time'] = 300
-        nr_kills = 7
-        metazoo.register['nr_kills'] = nr_kills
-        metazoo.register['kills'] = [random.randint(0, self.num_servers()-1) for x in range(nr_kills)]
-        print('Running for {}s, killing: {}'.format(metazoo.register['time'], metazoo.register['kills']), flush=True)
+        metazoo.register['time'] = 5
         print('''
-Attention! In this experiment, we periodically kill and reboot servers.
-Some exceptions will appear in your terminal. Do not be alarmed.
-Here we give some of the occuring errors, which are due to killing and not important:
- 1. "KeeperErrorCode = ConnectionLoss for /ClientNode"
- 2. "[Thread-18:QuorumCnxManager$SendWorker@559] - Failed to send last message.
-     Shutting down thread. java.nio.channels.ClosedChannelException"
- 3. "[LearnerHandler-/10.149.0.27:58498:LearnerHandler@444] - Unexpected exception
-     causing shutdown while sock still open"
-
 Attention! Due to the large amount of clients constantly reading and writing data,
 snapshots and logs grow quickly to terrabyte-scale. Due to storage quota,
 we periodically remove old logs and snapshots.
@@ -94,7 +62,7 @@ Happy experimenting!
         return 'java -Dlog4j.configuration=file:"{}" "-Dprops={}" -jar {} {} {} {}'.format(
         fs.join(loc.get_client_cfg_dir(), 'log4j.properties'),
         'ERROR, CONSOLE',
-        fs.join(loc.get_metazoo_dep_dir(), 'faulttolerance_client', 'zookeeper-client.jar'),
+        fs.join(loc.get_metazoo_dep_dir(), 'throughput_client', 'zookeeper-client.jar'),
         balancer.balance_simple(metazoo.hosts, metazoo.gid),
         metazoo.gid,
         fs.join(loc.get_node_log_dir(), '{}.log'.format(metazoo.gid)))
@@ -106,20 +74,10 @@ Happy experimenting!
         time.sleep(sleep_time) #Client remains active for a while
         metazoo.executor.stop()
 
-
     def experiment_server(self, metazoo):
         '''Execution occuring on ALL server nodes'''
-        nap_time = metazoo.register['time'] / (metazoo.register['nr_kills']+1)
-        kills = metazoo.register['kills']
-        time.sleep(nap_time)
-
-        for kill in kills:
-            if kill == metazoo.gid:
-                with open(fs.join(metazoo.log_location, 'kills.log'), 'a') as file:
-                    file.write('{},{},{}\n'.format(tm.timestamp('%Y-%m-%d_%H:%M:%S.%f'), kill, metazoo.is_leader))
-                metazoo.executor.reboot()
-            time.sleep(nap_time)
-
+        sleep_time = metazoo.register['time']
+        time.sleep(sleep_time)
 
     def post_experiment(self, metazoo):
         print('Experiments are done')
---
+++ zookeeper-client/src/ThroughputClient.java
@@ -0,0 +1,106 @@
+import org.apache.log4j.Logger;
+import org.apache.zookeeper.AsyncCallback;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ThroughputClient {
+    private static String logfile;
+    private static int operations;
+    private static ZooKeeperConnection conn;
+    private static ZnodeManager nodes;
+    private static String node_name;
+    private static Logger LOG;
+
+    private static int NR_READS;
+    private static int NR_WRITES;
+    private static final int OUTGOING = 100;
+    private static final int BYTES = 1024;
+
+    public static void run() throws KeeperException, InterruptedException {
+        byte[] data = new byte[BYTES];
+        for (int i = 0; i < BYTES; ++i)
+            data[i] = (byte)i;
+
+        if (!nodes.exists(node_name))
+            nodes.create(node_name, data);
+
+        class MyDataCallback implements AsyncCallback.DataCallback {
+            @Override
+            public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+                ++operations;
+                nodes.getData_async(node_name, this);
+            }
+        }
+
+        class MyStatCallback implements AsyncCallback.StatCallback {
+            @Override
+            public void processResult(int rc, String path, Object ctx, Stat stat) {
+                try {
+                    ++operations;
+                    nodes.setData_async(node_name, data, this);
+                } catch (Exception ignored) {
+                    //LOG.error(e.getMessage());
+                }
+            }
+        }
+
+        MyDataCallback db = new MyDataCallback();
+        MyStatCallback sb = new MyStatCallback();
+
+        for (int i = 0; i < NR_READS; ++i)
+            nodes.getData_async(node_name, db);
+        for (int i = 0; i < NR_WRITES; ++i)
+            nodes.setData_async(node_name, data, sb);
+
+        //waiting for process to be quit
+        while (true) ;
+    }
+
+    public static void shutdown() {
+        try {
+            nodes.delete(node_name);
+            conn.close();
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+        try {
+            FileWriter writer = new FileWriter(logfile);
+            writer.write("ops: "+operations+"\n");
+            writer.close();
+        } catch (IOException ignored) {
+            //LOG.error(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println("[CLIENT] [ERROR] expected four arguments");
+            System.exit(1);
+        }
+        String host = args[0];
+        int id = Integer.parseInt(args[1]);
+        logfile = args[2];
+        NR_READS = Integer.parseInt(args[3]);
+        NR_WRITES = OUTGOING - NR_READS;
+        operations = 0;
+        conn = new ZooKeeperConnection();
+        try {
+            LOG = Logger.getLogger(FailureClient.class);
+            node_name = "/ClientNode" + id;
+            String message = "I am Client"+ id;
+            LOG.warn(message);
+            Runtime.getRuntime().addShutdownHook(new Thread(FailureClient::shutdown));
+            ZooKeeper zoo = conn.connect(host);
+            nodes = new ZnodeManager(zoo);
+            run();
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        } finally {
+            shutdown();
+        }
+    }
+}
