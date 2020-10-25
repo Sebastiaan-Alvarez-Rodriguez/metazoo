5bb0af8af068983e160c574f4e49aed7a96e0501
Mariska <mariska-ijpelaar@hotmail.com>
2020-10-25 15:33:07 +0100

    WIP: throughput statistics

--- metazoo/src/result/throughput/assembler.py
+++ metazoo/src/result/throughput/assembler.py
@@ -12,4 +12,4 @@ class Assembler(object):
     def read_ops(self):
         for run in fs.ls(self._path, only_dirs=True, full_paths=True):
             reader = Reader(run)
-            yield [(x[0][0], sum(n for _, n in x)) for x in zip(*[reader.read_ops(x) for x in range(reader.num_files)])]
+            yield ((x[0][0], sum(n for _, n in x)) for x in zip(*[reader.read_ops(x) for x in range(reader.num_files)]))
--- metazoo/src/result/throughput/gen.py
+++ metazoo/src/result/throughput/gen.py
@@ -2,26 +2,43 @@ from collections import defaultdict
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-#from result.throughput.assembler import Assembler
+from result.throughput.assembler import Assembler
 from result.throughput.reader import Reader
 import result.util.storer as storer 
 import util.fs as fs
 import util.location as loc 
 from util.printer import *
 
-#TODO: current version is for one run only. 
 def throughput(logdir, large, no_show, store_fig, filetype, original):
     seconds = 5
     if original:
         printe('not implemented yet')
     else: 
         path = fs.join(loc.get_metazoo_results_dir(), logdir)
-        run = next(fs.ls(path, only_dirs=True, full_paths=True))
-        reader = Reader(run)
-        frame = [(x[0][0], sum(n for _, n in x)) for x in zip(*[reader.read_ops(x) for x in range(reader.num_files)])]
+        assembler = Assembler(path)
+        frames = [(x[0][0], [n for _, n in x]) for x in zip(*list(assembler.read_ops()))]
+        data = [x for _, x in frames]
+        ticks = ['']
+        ticks.extend([x for x, _ in frames])
+        #frame = ((x[0][0], sum(n for _, n in x)) for x in zip(*[reader.read_ops(x) for x in range(reader.num_files)]))
         fig, ax = plt.subplots()
-        for ratio, ops in frame: 
-            ax.scatter(ratio, ops / seconds, color='tab:green', marker='o')
+        ax.boxplot(data)
+        plt.xticks(np.arange(len(ticks)+1), labels=ticks)
+
+        # axes = [None for x in range(5)]
+        # axes[0] = plt.subplot(231)
+        # axes[0].tick_params(axis='y', direction='in', pad=-40)
+        # axes[1] = plt.subplot(232, sharex=axes[0])
+        # axes[1].tick_params(axis='y', direction='in', pad=-40)
+        # axes[2] = plt.subplot(233, sharex=axes[0])
+        # axes[2].tick_params(axis='y', direction='in', pad=-40)
+        # axes[3] = plt.subplot(234)
+        # axes[3].tick_params(axis='y', direction='in', pad=-40)
+        # axes[4] = plt.subplot(235, sharex=axes[3])
+        # axes[4].tick_params(axis='y', direction='in', pad=-40)
+        # for i, x in enumerate(data):
+        #     axes[i].boxplot(x)
+        
         ax.set(yscale='log', xlabel='Read Ratio', ylabel='Operations per Second', title='throughput')
 
     if store_fig:
