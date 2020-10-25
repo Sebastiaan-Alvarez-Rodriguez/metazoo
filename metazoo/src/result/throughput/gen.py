e2283b69f5720c949cf816abf1319af7617e768b
Mariska <mariska-ijpelaar@hotmail.com>
2020-10-25 19:46:29 +0100

    WIP: throughput original is implemented, not tested

--- metazoo/src/result/throughput/gen.py
+++ metazoo/src/result/throughput/gen.py
@@ -12,10 +12,33 @@ from util.printer import *
 
 def throughput(logdir, large, no_show, store_fig, filetype, original):
     seconds = 5
+    path = fs.join(loc.get_metazoo_results_dir(), logdir)
+
+    if large: 
+        fontsize = 16
+        font = {
+            'family' : 'DejaVu Sans',
+            'weight' : 'bold',
+            'size'   : fontsize
+        }
+        plt.rc('font', **font)
+
+
     if original:
         printe('not implemented yet')
-    else:
-        path = fs.join(loc.get_metazoo_results_dir(), logdir)
+        runs = sorted(list(fs.ls(path, only_dirs=True, full_paths=True)))
+        servers = []
+        for run in runs:
+            reader = Reader(run)
+            servers.append((x[0][0], sum(n for _, n in x)) for x in zip(*[reader.read_ops(x) for x in range(reader.num_files)]))
+        fig, ax = plt.subplots()
+        colors = ['r', 'lime', 'b', 'magenta', 'cyan']
+        numbers = ['3', '5', '7', '9', '13']
+        linestyles = ('solid', 'dashed', 'dotted', (0, (1,1)), 'dashdot')
+        for server, color, i, linestyle in zip(servers, colors, numbers, linestyles):
+            ax.plot([x for x,_ in server], [x for _, x in server], color=color, linestyle=linestyle, label='{} servers'.format(i))
+
+    else:  
         assembler = Assembler(path)
         frames = [(x[0][0], [n for _, n in x]) for x in zip(*list(assembler.read_ops()))]
         
@@ -33,29 +56,24 @@ def throughput(logdir, large, no_show, store_fig, filetype, original):
         data = [x for _, x in frames]
         ticks = ['']
         ticks.extend([x for x, _ in frames])
-        #frame = ((x[0][0], sum(n for _, n in x)) for x in zip(*[reader.read_ops(x) for x in range(reader.num_files)]))
         fig, ax = plt.subplots()
-        ax.boxplot(data)
+        bplot = ax.boxplot(data, patch_artist=True)
+        plt.setp(bplot['boxes'], color='lightgreen')
+        plt.setp(bplot['boxes'], edgecolor='black')
+        plt.setp(bplot['medians'], color='forestgreen')
         plt.xticks(np.arange(len(ticks)+1), labels=ticks)
-
-        # axes = [None for x in range(5)]
-        # axes[0] = plt.subplot(231)
-        # axes[0].tick_params(axis='y', direction='in', pad=-40)
-        # axes[1] = plt.subplot(232, sharex=axes[0])
-        # axes[1].tick_params(axis='y', direction='in', pad=-40)
-        # axes[2] = plt.subplot(233, sharex=axes[0])
-        # axes[2].tick_params(axis='y', direction='in', pad=-40)
-        # axes[3] = plt.subplot(234)
-        # axes[3].tick_params(axis='y', direction='in', pad=-40)
-        # axes[4] = plt.subplot(235, sharex=axes[3])
-        # axes[4].tick_params(axis='y', direction='in', pad=-40)
-        # for i, x in enumerate(data):
-        #     axes[i].boxplot(x)
         
-        ax.set(yscale='log', xlabel='Read Ratio', ylabel='Operations per Second', title='throughput')
+        ax.set(yscale='log', xlabel='Read Ratio', ylabel='Operations per Second', title='Throughput')
+
+    fig.tight_layout()
+    if large:
+        fig.set_size_inches(8, 6)
 
     if store_fig:
        storer.store('throughput', logdir, filetype, plt)
+
+    if large:
+        plt.rcdefaults()
 
     if not no_show:
         plt.show()
